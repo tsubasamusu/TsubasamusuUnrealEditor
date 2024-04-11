a16d3609abf1913a8dedf04b0471c98883b59b62
tsubasamusu <tsubasa_03_08@icloud.com>
2024-04-12 06:40:31 +0900

    FTsubasamusuUnrealEditor 内の不要な関数のオーバーライドを削除

--- TsubasamusuUnrealEditor.cpp
+++ TsubasamusuUnrealEditor.cpp
@@ -1,13 +1,3 @@
 #include "TsubasamusuUnrealEditor.h"
 
 IMPLEMENT_GAME_MODULE(FTsubasamusuUnrealEditor, TsubasamusuUnrealEditor);
-
-void FTsubasamusuUnrealEditor::StartupModule()
-{
-
-}
-
-void FTsubasamusuUnrealEditor::ShutdownModule()
-{
-
-}
